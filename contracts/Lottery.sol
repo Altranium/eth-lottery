219e41993c50d0537f0fcbf37eaba297fb6dd910
Alan Tran <dev@protoalpha.co>
2020-03-27 22:51:06 -0500

    Add random generator and pick winner function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -10,9 +10,19 @@ contract Lottery {
     }
 
     function enter() public payable {
-        require(msg.value > .01 ether);
-        
+        require(msg.value > .01 ether, 'Account most fund more than .01 ether');
+
         players.push(msg.sender);
     }
 
+    //! Solidity can only handle pseudo-random and does not have random function built-in
+    //! It's not truely random because there is preset knowledge that is fed in before the number is generated
+    function random() private view returns(uint) {
+        return uint(keccak256(block.difficulty, block.timestamp, players));    // Uses block & now global variable
+    }
+
+    function pickWinner() public {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);      // Transfer the contract's balance to the winner
+    }
 }
