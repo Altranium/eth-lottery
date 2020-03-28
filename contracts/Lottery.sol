97cc6cb7a101f585ef9b1273375933aaa716b723
Alan Tran <dev@protoalpha.co>
2020-03-27 23:13:40 -0500

    Fix warnings

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -18,11 +18,11 @@ contract Lottery {
     //! Solidity can only handle pseudo-random and does not have random function built-in
     //! It's not truely random because there is preset knowledge that is fed in before the number is generated
     function random() private view returns(uint) {
-        return uint(keccak256(block.difficulty, block.timestamp, players));    // Uses block & now global variable
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));    // Uses block & now global variable
     }
 
     function pickWinner() public {
         uint index = random() % players.length;
-        players[index].transfer(this.balance);      // Transfer the contract's balance to the winner
+        players[index].transfer(address(this).balance);      // Transfer the contract's balance to the winner
     }
 }
